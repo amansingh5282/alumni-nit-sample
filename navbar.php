e23e32cfb8aa4b239f3dc68d46bf4bfc681f683a
aman\admin <amanterry20000@gmail.com>
2016-12-24 15:08:50 +0530

    Created blank page and empty index page

---
+++ navbar.php
@@ -0,0 +1,82 @@
+<!--div class="header mdl-layout__header mdl-layout__header--waterfall">
+        <div class="mdl-layout__header-row">
+          <span class="title mdl-layout-title">
+            <img class="logo-image" src="images/INAR_LOGO_ONLY.jpg" >
+          </span>
+          <!-- Add spacer, to align navigation to the right in desktop -->
+          <!--div class="header-spacer mdl-layout-spacer"></div-->
+          <!--div class="search-box mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right mdl-textfield--full-width">
+            <label class="mdl-button mdl-js-button mdl-button--icon" for="search-field">
+              <i class="material-icons">search</i>
+            </label>
+            <div class="mdl-textfield__expandable-holder">
+              <input class="mdl-textfield__input" type="text" id="search-field">
+            </div>
+          </div-->
+          <!-- Navigation -->
+          <!--div class="navigation-container">
+            <nav class="navigation mdl-navigation">
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Home</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">About Us</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Gallery</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Publications</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">News</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Events</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Team</a>
+            </nav>
+          </div>
+          <span class="mobile-title mdl-layout-title">
+            <img class="logo-image" src="images/logo.png">
+          </span>
+          <button class="more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
+            <i class="material-icons">more_vert</i>
+          </button>
+          <ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect" for="more-button">
+            <li class="mdl-menu__item">Contact</li>
+            <li class="mdl-menu__item">How to Rezch</li>
+            <!--li disabled class="mdl-menu__item">4.3 Jelly Bean</li>
+            <li class="mdl-menu__item">Android History</li>
+          </ul>
+        </div-->
+        <header class="header mdl-layout__header">
+    <div class="mdl-layout__header-row">
+      <!-- Title -->
+      <span class="title mdl-layout-title">
+            <img class="logo-image" src="images/INAR_LOGO_ONLY.jpg" >
+      </span>
+      <span class="mdl-layout-title" >&nbsp; INDUSTRY AND ALUMNI RELATIONS</span>
+      <!-- Add spacer, to align navigation to the right -->
+      <div class="mdl-layout-spacer"></div>
+      <!-- Navigation. We hide it in small screens. -->
+      <nav class="navigation mdl-navigation mdl-layout--large-screen-only">
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Home</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">About Us</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Gallery</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Publications</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">News</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Events</a>
+              <a class="mdl-navigation__link mdl-typography--text-uppercase" href="">Team</a>
+            </nav>
+            <button class="mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
+            <i class="mdi mdi-dots-horizontal" style="color:black"></i>
+          </button>
+    </div>
+    
+          <ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect" for="more-button">
+            <li class="mdl-menu__item">Contact Us</li>
+            <li class="mdl-menu__item">How to Reach</li>
+            <!--li disabled class="mdl-menu__item">4.3 Jelly Bean</li>
+            <li class="mdl-menu__item">Android History</li-->
+          </ul>
+  </header>
+  <div class="mdl-layout__drawer">
+    <span class="mdl-layout-title" style="padding-top:20px;line-height: 25px;">Industry &amp; Alumni Relations</span>
+    <nav class="mdl-navigation">
+      <a class="mdl-navigation__link" href="">Home</a>
+      <a class="mdl-navigation__link" href="">About Us</a>
+      <a class="mdl-navigation__link" href="">Gallery</a>
+      <a class="mdl-navigation__link" href="">Publications</a>
+      <a class="mdl-navigation__link" href="">News</a>
+      <a class="mdl-navigation__link" href="">Events</a>
+      <a class="mdl-navigation__link" href="">Team</a>
+    </nav></div>
