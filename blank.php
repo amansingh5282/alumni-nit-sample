88546f4751f8be8d1ab285622fe594056a4939f4
amansingh5282 <Amanterry2000@yahoo.com>
2016-12-24 15:29:21 +0530

    First template push (#2)
    * Created blank page and empty index page
    
    * Update README.md

---
+++ blank.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<?php
+include "header.php";
+?>
+<body>
+<div id="wrapper" class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+<?php
+include "navbar.php";
+?>
+<?php
+include "footer.php"; 
+?>
+</div>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,111 @@
+
+        <!--footer class="footer mdl-mega-footer">
+          <div class="mdl-mega-footer--top-section">
+            <div class="mdl-mega-footer--right-section">
+              <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                <i class="mdi mdi-facebook"></i>
+              </button>
+              &nbsp;
+              <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                <i class="mdi mdi-youtube-play"></i>
+              </button>
+              &nbsp;
+              <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                <i class="mdi mdi-linkedin"></i>
+              </button>
+            </div> 
+          </div>
+
+          <div class="mdl-mega-footer--middle-section">
+            <p class="mdl-typography--font-light">Satellite imagery: © 2014 Astrium, DigitalGlobe</p>
+            <p class="mdl-typography--font-light">Some features and devices may not be available in all areas</p>
+          </div>
+
+          <div class="mdl-mega-footer--bottom-section">
+            <a class="link link-menu mdl-typography--font-light" id="version-dropdown">
+              Alumni Meet-2017
+              <i class="material-icons">arrow_drop_up</i>
+            </a>
+            <ul class="mdl-menu mdl-js-menu mdl-menu--top-left mdl-js-ripple-effect" for="version-dropdown">
+              <li class="mdl-menu__item">Register</li>
+              <li class="mdl-menu__item">Hotels List</li>
+              <li class="mdl-menu__item">FAQ's</li>
+            </ul>
+            <a class="link link-menu mdl-typography--font-light" id="developers-dropdown">
+              For Developers
+              <i class="material-icons">arrow_drop_up</i>
+            </a>
+            <ul class="mdl-menu mdl-js-menu mdl-menu--top-left mdl-js-ripple-effect" for="developers-dropdown">
+              <li class="mdl-menu__item">App developer resources</li>
+              <li class="mdl-menu__item">Android Open Source Project</li>
+              <li class="mdl-menu__item">Android SDK</li>
+              <li class="mdl-menu__item">Android for Work</li>
+            </ul>
+            <a class="link mdl-typography--font-light" href="">Blog</a>
+            <a class="link mdl-typography--font-light" href="">Privacy Policy</a>
+          </div>
+
+        </footer-->
+    <footer class="mdl-mega-footer" id="bottom-footer">
+      <div class="mdl-mega-footer__top-section"></div>
+  <div class="mdl-mega-footer__middle-section">
+
+    <div class="mdl-grid" id="footer-content">
+      <div class="mdl-cell--4-col" style="border-right:1px dashed white">
+       <h3>INDUSTRY &amp; ALUMNI RELATIONS</h3>
+       <blockquote><h6><span style="color:#ff4081;">CONNECT</span> &nbsp;Today<br/><span style="color:#ff4081;">TRANSFORM</span> Tomorrow<br/>It's not our mission<br/>It's our wish <span style="color:#ff4081;">FOR YOU</span></h6></blockquote>
+      <a href="mailto:alumniteam@nitjsr.ac.in">alumniteam@nitjsr.ac.in</a>
+      </div>
+
+    <div class="mdl-cell--4-col" style="padding-left:20px">
+      <h3>LINKS</h3>
+      <ul class="mdl-mega-footer__link-list">
+        <li><a href="#">AMRIT - NIT Jamshedpur Newsletter</a></li>
+        <li><a href="#">Itinerary</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Publications</a></li>
+        <li><a href="#">NIT Jamshedpur Website</a></li>
+        <li><a href="#">Team</a></li>
+        <li><a href="#">Contact Us</a></li>
+      </ul>
+
+    </div>
+
+    <div class="mdl-cell--4-col" style="padding-left:20px">
+      <h3>ADDRESS</h3>
+      <p> Office of the Dean , Industry and Alumni Relations ,<br/>
+        Administrative Building , <br/>
+        National Institute of Technology , Adityapur, <br/>
+        Jamshedpur-831014<br/>
+        Ph: +91-9430738551<br/>
+        <a href="mailto:sanjay.me@nitjsr.ac.in">sanjay.me@nitjsr.ac.in</a>
+
+    </div>
+
+    
+
+  </div>
+  </footer>
+  <footer class="mdl-mega-footer" style="background:rgba(0,0,0,.87);">
+
+  <div class="mdl-mega-footer__bottom-section">
+    <div class="mdl-grid">
+        <div class="mdl-cell--5-offset mdl-cell--1-col" >
+           <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                <i class="mdi mdi-facebook"></i>
+              </button>
+        </div>
+        <div class="mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                <i class="mdi mdi-youtube-play"></i>
+              </button></div>
+        <div class="mdl-cell--1-col"><button class="mdl-button mdl-js-button mdl-button--fab mdl-button--colored">
+                <i class="mdi mdi-linkedin"></i>
+              </button></div>
+    </div>
+    <div class="mdl-grid">
+      <div class="mdl-cell--5-offset mdl-cell--5-col">2016 &copy; Developed by INAR Webteam </div>
+</div>
+
+
+    <!--a href="https://github.com/google/material-design-lite/blob/mdl-1.x/templates/dot-com/" target="_blank" id="view-source" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--accent mdl-color-text--accent-contrast">View Source</a-->
+    <script src="js/material.min.js"></script>
